4dafc4cb52967a3488c9c110fd9feb4e1d99f47c
Yordanos Teferi <jordisavage90@gmail.com>
2025-10-01 17:44:28 +0300

    Update

--- online_poll_backend/online_poll_backend/settings.py
+++ online_poll_backend/online_poll_backend/settings.py
@@ -52,7 +52,7 @@ DB_PORT = os.getenv("DB_PORT")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
@@ -174,8 +174,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
