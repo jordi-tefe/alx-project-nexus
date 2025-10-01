49c6ac42c300e13d928868df2dcff5939b3b8481
Yordanos Teferi <jordisavage90@gmail.com>
2025-10-01 23:42:27 +0300

    showing routes in home

--- online_poll_backend/online_poll_backend/views.py
+++ online_poll_backend/online_poll_backend/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to ALX Project Nexus Backend!")
+    return HttpResponse("""
+        <h1>Welcome to ALX Project Nexus Backend 🚀</h1>
+        <p>Here are some available routes:</p>
+        <ul>
+            <li><a href="/admin/">Admin Panel</a> (login with your admin username & password)</li>
+        </ul>
+    """)
